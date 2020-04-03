1cc5b564bd97f6520921d9277d70c52abaa6fcbc
Dani <d.mittmann@devnet.de>
2020-04-03 17:51:09 +0200

    Changes

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,8 +3,14 @@ import uploadcare from "netlify-cms-media-library-uploadcare";
 import cloudinary from "netlify-cms-media-library-cloudinary";
 
 import IndexPagePreview from "./preview-templates/IndexPagePreview";
+import IndexPagePreview from "./preview-templates/IndexPagePreview";
+import IndexPagePreview from "./preview-templates/IndexPagePreview";
+import IndexPagePreview from "./preview-templates/IndexPagePreview";
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
+CMS.registerPreviewTemplate("index", IndexPagePreview);
+CMS.registerPreviewTemplate("index", IndexPagePreview);
+CMS.registerPreviewTemplate("index", IndexPagePreview);
 CMS.registerPreviewTemplate("index", IndexPagePreview);
