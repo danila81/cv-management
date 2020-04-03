995b3fd4cf21ec0d06f2a68d237a28ccca6a0523
Dani <d.mittmann@devnet.de>
2020-04-03 17:55:26 +0200

    next

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,14 +3,14 @@ import uploadcare from "netlify-cms-media-library-uploadcare";
 import cloudinary from "netlify-cms-media-library-cloudinary";
 
 import IndexPagePreview from "./preview-templates/IndexPagePreview";
-import IndexPagePreview from "./preview-templates/IndexPagePreview";
-import IndexPagePreview from "./preview-templates/IndexPagePreview";
-import IndexPagePreview from "./preview-templates/IndexPagePreview";
+import IndexPagePreview from "./preview-templates/ProductPagePreview";
+import IndexPagePreview from "./preview-templates/BlogPostPreview";
+import IndexPagePreview from "./preview-templates/AboutPagePreview";
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate("index", IndexPagePreview);
-CMS.registerPreviewTemplate("index", IndexPagePreview);
-CMS.registerPreviewTemplate("index", IndexPagePreview);
-CMS.registerPreviewTemplate("index", IndexPagePreview);
+CMS.registerPreviewTemplate("index", ProductPagePreview);
+CMS.registerPreviewTemplate("index", BlogPostPreview);
+CMS.registerPreviewTemplate("index", AboutPagePreview);
